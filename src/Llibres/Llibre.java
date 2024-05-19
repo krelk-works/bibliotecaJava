a39bd4446da9019b51d38d0e33acc9f783e5b679
krelk_works <leo.craciun97@gmail.com>
2024-05-19 14:07:13 +0200

    Getters i Setters implementats a les classes basiques

--- src/Llibres/Llibre.java
+++ src/Llibres/Llibre.java
@@ -1,7 +1,7 @@
 package Llibres;
 
 public class Llibre {
-    private int id;
+    private int id = 0;
     private String titol;
     private String autor;
     private String isbn;
@@ -9,5 +9,99 @@ public class Llibre {
     private int anyPublicacio;
     private String categoria;
     private String estat;
-    // Pendent : Getters i setters
+
+    // Aquest constructor és necesari per a que poguem treballar amb el llibre i la connexió a la base de dades posteriorment
+    public Llibre(int id, String titol, String autor, String isbn, String editorial, int anyPublicacio, String categoria, String estat) {
+        this.id = id;
+        this.titol = titol;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.editorial = editorial;
+        this.anyPublicacio = anyPublicacio;
+        this.categoria = categoria;
+        this.estat = estat;
+    }
+
+    // Aquest constructor és necesari per quan necessitem afegir un nou llibre a la base de dades, per exemple
+    public Llibre(String titol, String autor, String isbn, String editorial, int anyPublicacio, String categoria, String estat) {
+        this.titol = titol;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.editorial = editorial;
+        this.anyPublicacio = anyPublicacio;
+        this.categoria = categoria;
+        this.estat = estat;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitol() {
+        return this.titol;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getEditorial() {
+        return this.editorial;
+    }
+
+    public int getAnyPublicacio() {
+        return this.anyPublicacio;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public String getEstat() {
+        return this.estat;
+    }
+
+    // De moment els Setters no són necessaris, ja que no tenim cap funció que modifiqui les dades del llibre
+
+    /*
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public void setAutorautor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public void setAnyPublicacio(int anyPublicacio) {
+        this.anyPublicacio = anyPublicacio;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setEstat(String estat) {
+        this.estat = estat;
+    }
+    */
+
+    public String toString() {
+        return "Llibre [id=" + id + ", titol=" + titol + ", autor=" + autor + ", isbn=" + isbn + ", editorial=" + editorial + ", anyPublicacio=" + anyPublicacio + ", categoria=" + categoria + ", estat=" + estat + "]";
+    }
 }
--- src/Prestecs/Prestec.java
+++ src/Prestecs/Prestec.java
@@ -3,12 +3,96 @@ package Prestecs;
 import java.sql.Date;
 
 public class Prestec {
-    private int id;
+    private int id = 0;
     private int idLlibre;
     private int idUsuari;
     private Date dataPrestec;
     private Date dataRetornPrevista;
     private Date dataRetornReal;
     private String estat;
-    // Pendent : Getters i setters
+
+    // Constructor necesari per fer operacions amb la base de dades
+    public Prestec(int id, int idLlibre, int idUsuari, Date dataPrestec, Date dataRetornPrevista, Date dataRetornReal, String estat) {
+        this.id = id;
+        this.idLlibre = idLlibre;
+        this.idUsuari = idUsuari;
+        this.dataPrestec = dataPrestec;
+        this.dataRetornPrevista = dataRetornPrevista;
+        this.dataRetornReal = dataRetornReal;
+        this.estat = estat;
+    }
+
+    // Constructor necesari per afegir un nou prestec a la base de dades, per exemple.
+    public Prestec(int idUsuari, int idLlibre, Date dataPrestec, Date dataRetornPrevista, String estat) {
+        this.idUsuari = idUsuari;
+        this.idLlibre = idLlibre;
+        this.dataPrestec = dataPrestec;
+        this.dataRetornPrevista = dataRetornPrevista;
+        this.estat = estat;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getIdLlibre() {
+        return this.idLlibre;
+    }
+
+    public int getIdUsuari() {
+        return this.idUsuari;
+    }
+
+    public Date getDataPrestec() {
+        return this.dataPrestec;
+    }
+
+    public Date getDataRetornPrevista() {
+        return this.dataRetornPrevista;
+    }
+
+    public Date getDataRetornReal() {
+        return this.dataRetornReal;
+    }
+
+    public String getEstat() {
+        return this.estat;
+    }
+
+    // Deshabilitem els setters perquè no tenim cap necessitat d'utilitzar-los. Els deixem creats per si en un futur són necessaris.
+
+    /*
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIdLlibre(int idLlibre) {
+        this.idLlibre = idLlibre;
+    }
+
+    public void setIdUsuari(int idUsuari) {
+        this.idUsuari = idUsuari;
+    }
+
+    public void setDataPrestec(Date dataPrestec) {
+        this.dataPrestec = dataPrestec;
+    }
+
+    public void setDataRetornPrevista(Date dataRetornPrevista) {
+        this.dataRetornPrevista = dataRetornPrevista;
+    }
+
+    public void setDataRetornReal(Date dataRetornReal) {
+        this.dataRetornReal = dataRetornReal;
+    }
+
+    public void setEstat(String estat) {
+        this.estat = estat;
+    }
+    */
+
+    public String toString() {
+        return "Prestec [id=" + id + ", idLlibre=" + idLlibre + ", idUsuari=" + idUsuari + ", dataPrestec=" + dataPrestec
+                + ", dataRetornPrevista=" + dataRetornPrevista + ", dataRetornReal=" + dataRetornReal + ", estat=" + estat + "]";
+    }
 }
