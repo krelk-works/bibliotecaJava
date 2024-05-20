b189eccc71016a79eeafa39cefe94bc8f9d3db3e
krelk_works <leo.craciun97@gmail.com>
2024-05-21 00:32:31 +0200

    Implementació de columna de DATA DE PRESTEC per a la Consulta de l'històric de Préstecs dels Lectors

--- src/Prestecs/ConsultaPrestecs.java
+++ src/Prestecs/ConsultaPrestecs.java
@@ -4,8 +4,8 @@ import Connexio.Connexio;
 import Usuaris.Usuari;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -31,17 +31,17 @@ public class ConsultaPrestecs extends JFrame {
         panel.setLayout(new BorderLayout());
 
         // Crear la taula per llistar els préstecs
-        tableModel = new DefaultTableModel(new Object[]{"Títol", "Autor", "Editorial", "Categoria", "Estat"}, 0);
+        tableModel = new DefaultTableModel(new Object[]{"Títol", "Autor", "Editorial", "Categoria", "Data Préstec", "Estat"}, 0);
         table = new JTable(tableModel) {
             @Override
             public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
                 Component c = super.prepareRenderer(renderer, row, column);
-                String estat = (String) getValueAt(row, 4);
+                String estat = (String) getValueAt(row, 5);
                 if ("retardat".equals(estat)) {
-                    c.setBackground(Color.RED);
-                    c.setForeground(Color.WHITE);
+                    if (column == 5) {
+                        c.setForeground(Color.RED);
+                    }
                 } else {
-                    c.setBackground(Color.WHITE);
                     c.setForeground(Color.BLACK);
                 }
                 return c;
@@ -52,7 +52,8 @@ public class ConsultaPrestecs extends JFrame {
         table.getColumnModel().getColumn(1).setPreferredWidth(150); // Autor
         table.getColumnModel().getColumn(2).setPreferredWidth(150); // Editorial
         table.getColumnModel().getColumn(3).setPreferredWidth(100); // Categoria
-        table.getColumnModel().getColumn(4).setPreferredWidth(100); // Estat
+        table.getColumnModel().getColumn(4).setPreferredWidth(150); // Data Préstec
+        table.getColumnModel().getColumn(5).setPreferredWidth(100); // Estat
 
         // Omplir la taula amb dades de la base de dades
         omplirTaulaPrestecs();
@@ -68,12 +69,11 @@ public class ConsultaPrestecs extends JFrame {
     private void omplirTaulaPrestecs() {
         tableModel.setRowCount(0); // Esborrar les files existents
         try (Connection connection = Connexio.getConnection();
-            PreparedStatement statement = connection.prepareStatement(
-                    "SELECT l.Títol, l.Autor, l.Editorial, l.Categoria, p.Data_Retorn_Prevista, p.Data_Retorn_Real " +
-                            "FROM llibres l " +
-                            "INNER JOIN préstecs p ON l.ID_Llibre = p.ID_Llibre " +
-                            "WHERE p.ID_Usuari = ?")) {
-            // El jorge tendria que estar orgulloso c*llons!!!
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT l.Títol, l.Autor, l.Editorial, l.Categoria, p.Data_Préstec, p.Data_Retorn_Prevista, p.Data_Retorn_Real " +
+                             "FROM llibres l " +
+                             "INNER JOIN préstecs p ON l.ID_Llibre = p.ID_Llibre " +
+                             "WHERE p.ID_Usuari = ?")) {
             statement.setInt(1, usuari.getId());
             try (ResultSet resultSet = statement.executeQuery()) {
 
@@ -82,6 +82,7 @@ public class ConsultaPrestecs extends JFrame {
                     String autor = resultSet.getString("Autor");
                     String editorial = resultSet.getString("Editorial");
                     String categoria = resultSet.getString("Categoria");
+                    Date dataPrestec = resultSet.getDate("Data_Préstec");
                     Date dataRetornPrevista = resultSet.getDate("Data_Retorn_Prevista");
                     Date dataRetornReal = resultSet.getDate("Data_Retorn_Real");
 
@@ -101,7 +102,7 @@ public class ConsultaPrestecs extends JFrame {
                         }
                     }
 
-                    tableModel.addRow(new Object[]{titol, autor, editorial, categoria, estat});
+                    tableModel.addRow(new Object[]{titol, autor, editorial, categoria, dataPrestec, estat});
                 }
             }
         } catch (SQLException e) {
